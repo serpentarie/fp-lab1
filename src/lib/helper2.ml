796c7b6184e405bcc53bdc166a339f79d3ef3952
serpentarie <serpentariee@gmail.com>
2025-10-04 08:06:52 +0300

    helper2 modified

--- src/lib/helper2.ml
+++ src/lib/helper2.ml
@@ -1,14 +1,13 @@
 let cycle_length d =
   if d = 0 then 0
   else
-    let table = Hashtbl.create 128 in
-    let rec aux rem pos =
+    let rec aux rem pos seen =
       if rem = 0 then 0
       else
-        match Hashtbl.find_opt table rem with
+        match List.assoc_opt rem seen with
         | Some prev_pos -> pos - prev_pos
         | None ->
-            Hashtbl.replace table rem pos;
-            aux (rem * 10 mod d) (pos + 1)
+            let seen' = (rem, pos) :: seen in
+            aux (rem * 10 mod d) (pos + 1) seen'
     in
-    aux (1 mod d) 0
+    aux (1 mod d) 0 []
